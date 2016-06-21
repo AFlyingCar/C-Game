1f9285b5310dcae61c8af55fd4676da791d16c15
Tyler Robbins <tyler@familyrobbins.com>
2016-06-21 15:15:26 -0700

    Added a file for enums.

---
+++ src/enums.h
@@ -0,0 +1,24 @@
+#ifndef _ENUMS_H_
+#define _ENUMS_H_
+
+enum KeyPresses{
+    KEYFAIL=-1,
+    ARROWU=0,
+    ARROWD,
+    ARROWL,
+    ARROWR,
+    PRIMARY,
+    SECONDARY,
+    SHIFT,
+    PAUSE
+};
+
+enum Menu{
+    START_MENU,
+    CHARACTER_MENU,
+    PAUSE_MENU,
+    INGAME // The game counts as a menu
+};
+
+#endif
+
