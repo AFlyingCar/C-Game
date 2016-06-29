fadf20b8ef6c09be9eda73e27268d58bbc852842
Tyler Robbins <tyler@familyrobbins.com>
2016-06-29 10:21:13 -0700

    Added a file to store type definitions.

---
+++ src/types.h
@@ -0,0 +1,10 @@
+#ifndef _TYPES_H_
+#define _TYPES_H_
+
+typedef unsigned short uint8;
+typedef unsigned int uint16;
+typedef unsigned long int uint32;
+typedef unsigned long long int uint64;
+
+#endif
+
